b255ead748b3abeeed1c8e019e233201ffb0bc27
BarbaronaKJ <kenth.barbarona9@gmail.com>
2026-01-08 03:00:22 +0800

    Add campus map waypoints and building data (85% Completion)

--- pinsData.js
+++ pinsData.js
@@ -120,7 +120,7 @@ export const pins = [
     title: "20", 
     description: "BLDG 20 | Cafeteria", 
     image: require('./assets/USTP.jpg'),
-    neighbors: [1003]
+    neighbors: [1003.4, 1003.05]
   },
   { 
     id: 21, 
@@ -128,7 +128,7 @@ export const pins = [
     title: "21", 
     description: "BLDG 21 | Guard House", 
     image: require('./assets/USTP.jpg'),
-    neighbors: [1002]
+    neighbors: [1001]
   },
   { 
     id: 23, 
@@ -141,7 +141,7 @@ export const pins = [
   { 
     id: 24, x: 497, y: 580, title: "24", 
     description: "BLDG 24 | Girl's Trade Building", 
-    image: require('./assets/USTP.jpg'), neighbors: [1014] 
+    image: require('./assets/USTP.jpg'), neighbors: [1018.2] 
   },
   { 
     id: 25, 
@@ -149,13 +149,13 @@ export const pins = [
     title: "25", 
     description: "BLDG 25 | Food Innovation Center", 
     image: require('./assets/USTP.jpg'),
-    neighbors: [1015]
+    neighbors: [1016.2]
   },
   { 
     id: 27, x: 595, y: 700, 
     title: "27", 
     description: "BLDG 27 | University Health Center Building / Office of Student Affairs (OSA)", 
-    image: require('./assets/USTP.jpg'), neighbors: [1016] 
+    image: require('./assets/USTP.jpg'), neighbors: [1016.3] 
   },
   { 
     id: 28, 
@@ -163,31 +163,31 @@ export const pins = [
     title: "28", 
     description: "BLDG 28 | Old Science Building", 
     image: require('./assets/USTP.jpg'),
-    neighbors: [1016]
+    neighbors: [1014]
   },
   { 
     id: 35, x: 455, y: 755,
     title: "35", 
     description: "BLDG 35 | Old Education Building", 
-    image: require('./assets/USTP.jpg'), neighbors: [1016.1] 
+    image: require('./assets/USTP.jpg'), neighbors: [1018.1] 
   },
   { 
     id: 36, x: 410, y: 640, 
     title: "36", 
     description: "BLDG 36 | Old Student Center", 
-    image: require('./assets/USTP.jpg'), neighbors: [1016.1] 
+    image: require('./assets/USTP.jpg'), neighbors: [1018.2] 
   },
   { 
     id: 41, x: 750, y: 825, 
     title: "41", 
     description: "BLDG 41 | Science Complex", 
-    image: require('./assets/USTP.jpg'), neighbors: [1003] 
+    image: require('./assets/USTP.jpg'), neighbors: [1003.6] 
   },
   { 
     id: 42, x: 1100, y: 1095, 
     title: "42", 
     description: "BLDG 42 | Engineering Complex I (Right Wing)", 
-    image: require('./assets/USTP.jpg'), neighbors: [1003.3, 1005] 
+    image: require('./assets/USTP.jpg'), neighbors: [1003.3] 
   },
   { 
     id: 43, x: 980, y: 1108, 
@@ -206,14 +206,14 @@ export const pins = [
   { 
     id: 45, x: 360, y: 505, title: "45", 
     description: "BLDG 45 | Mechanical Laboratory Shop", 
-    image: require('./assets/USTP.jpg'), neighbors: [1014] 
+    image: require('./assets/USTP.jpg'), neighbors: [1020] 
   },
   { 
     id: 47, 
-    x: 500, y: 400, 
+    x: 490, y: 380, 
     title: "47", 
     description: "BLDG 47 | Technology Building", 
-    image: require('./assets/USTP.jpg'), neighbors: [1014] 
+    image: require('./assets/USTP.jpg'), neighbors: [] 
   },
   { 
     id: 50, x: 1220, y: 745, title: "50", isInvisible: true,
@@ -228,36 +228,64 @@ export const pins = [
   { 
     id: 52, x: 345, y: 405, title: "52", 
     description: "BLDG 52 | Fab Lab Building", 
-    image: require('./assets/USTP.jpg'), neighbors: [1014] 
+    image: require('./assets/USTP.jpg'), neighbors: [1021] 
+  },
+  { 
+    id: "MC", x: 970, y: 500, title: "MC", 
+    description: "MC | Movable Classrooms", 
+    image: require('./assets/USTP.jpg'), neighbors: [1016.1] 
   },
   // --- INVISIBLE WAYPOINTS (The Road Network) ---
   // Connects 0 -> 1001 -> 1002 ... -> 1007 -> 1
-  { title: "1001", id: 1001, x: 900, y: 1050, isInvisible: false, neighbors: [0, 1002] },
-  { title: "1001.1", id: 1001.1, x: 922, y: 1080, isInvisible: true, neighbors: [1] },
+  { title: "1001", id: 1001, x: 900, y: 1050, isInvisible: true, neighbors: [0, 1002] },
+  { title: "1001.1", id: 1001.1, x: 922, y: 1082, isInvisible: true, neighbors: [1] },
   { title: "1001.11", id: 1001.11, x: 954, y: 1068, isInvisible: true, neighbors: [1001.1] },
-  { title: "1001.2", id: 1001.2, x: 987, y: 1067, isInvisible: true, neighbors: [1001.11] },
-  { title: "1001.3", id: 1001.3, x: 998, y: 1095, isInvisible: true, neighbors: [1001.2] },
-  { title: "1002", id: 1002, x: 935, y: 990, isInvisible: false, neighbors: [1001, 1003] },
-  { title: "1003", id: 1003, x: 910, y: 910, isInvisible: false, neighbors: [1002, 1003.1,] },
-  { title: "1003.1", id: 1003.1, x: 1060, y: 850, isInvisible: false, neighbors: [1003, 1004,] },
-  { title: "1003.2", id: 1003.2, x: 1075, y: 920, isInvisible: false, neighbors: [1003.1] },
-  { title: "1003.3", id: 1003.3, x: 1100, y: 1030, isInvisible: false, neighbors: [1003.2] },
-  { title: "1004", id: 1004, x: 1125, y: 820, isInvisible: false, neighbors: [1003.1, 1005] },
-  { title: "1005", id: 1005, x: 1120, y: 770, isInvisible: false, neighbors: [1004, 1006] },
-  { title: "1005.1", id: 1005.1, x: 1205, y: 757, isInvisible: false, neighbors: [1005, 1005.2, 1006] },
-  { title: "1005.2", id: 1005.2, x: 1155, y: 725, isInvisible: false, neighbors: [1005.1] },
-  { title: "1006", id: 1006, x: 1260, y: 750, isInvisible: false, neighbors: [1005, 1007, 1006.1] },
-  { title: "1006.1", id: 1006.1, x: 1240, y: 690, isInvisible: false, neighbors: [1006, 1010] },
-  { title: "1007", id: 1007, x: 1440, y: 720, isInvisible: false, neighbors: [1006, 1008] },
-  { title: "1008", id: 1008, x: 1435, y: 650, isInvisible: false, neighbors: [1007, 1009] },
-  { title: "1009", id: 1009, x: 1525, y: 640, isInvisible: false, neighbors: [1008] },
-  { title: "1010", id: 1010, x: 1185, y: 630, isInvisible: false, neighbors: [1006.1] },
-  { title: "1011", id: 1011, x: 1110, y: 695, isInvisible: false, neighbors: [1005, 1005.2, 1012] },
-  { title: "1012", id: 1012, x: 1095, y: 595, isInvisible: false, neighbors: [1010, 1011] },
-  { title: "1013", id: 1013, x: 838, y: 720, isInvisible: false, neighbors: [1003] },
-  { title: "1013.1", id: 1013.1, x: 860, y: 710, isInvisible: false, neighbors: [1013] },
-  { title: "1013.2", id: 1013.2, x: 865, y: 680, isInvisible: false, neighbors: [1013.1] },
-  { title: "E", id: 1013.3, x: 895, y: 720, isInvisible: false, neighbors: [1013.1] },
+  { title: "1001.2", id: 1001.2, x: 983, y: 1064, isInvisible: true, neighbors: [1001.11] },
+  { title: "1001.3", id: 1001.3, x: 997, y: 1095, isInvisible: true, neighbors: [1001.2] },
+  { title: "1001.4", id: 1001.4, x: 1078, y: 1080, isInvisible: true, neighbors: [1001.3, 42] },
+  { title: "1002", id: 1002, x: 935, y: 990, isInvisible: true, neighbors: [1001, 1003] },
+  { title: "1003", id: 1003, x: 910, y: 910, isInvisible: true, neighbors: [1002, 1003.5] },
+  { title: "1003.05", id: 1003.05, x: 980, y:885, isInvisible: true, neighbors: [1003, 1003.1] },
+  { title: "1003.1", id: 1003.1, x: 1060, y: 850, isInvisible: true, neighbors: [1003, 1004] },
+  { title: "1003.2", id: 1003.2, x: 1075, y: 920, isInvisible: true, neighbors: [1003.1] },
+  { title: "1003.3", id: 1003.3, x: 1100, y: 1030, isInvisible: true, neighbors: [1001.4,1003.2] },
+  { title: "1003.4", id: 1003.4, x: 1051, y: 1016, isInvisible: true, neighbors: [1001.4, 1003.3] },
+  { title: "1003.5", id: 1003.5, x: 890, y: 860, isInvisible: true, neighbors: [1003] },
+  { title: "1003.6", id: 1003.6, x: 875, y: 820, isInvisible: true, neighbors: [1003.5,1013] },
+  { title: "1004", id: 1004, x: 1125, y: 820, isInvisible: true, neighbors: [1003.1, 1005] },
+  { title: "1005", id: 1005, x: 1120, y: 770, isInvisible: true, neighbors: [1004, 1006] },
+  { title: "1005.1", id: 1005.1, x: 1205, y: 757, isInvisible: true, neighbors: [1005, 1005.2, 1006] },
+  { title: "1005.2", id: 1005.2, x: 1155, y: 725, isInvisible: true, neighbors: [1005.1] },
+  { title: "1006", id: 1006, x: 1260, y: 750, isInvisible: true, neighbors: [1005.1, 1007, 1006.1] },
+  { title: "1006.1", id: 1006.1, x: 1240, y: 690, isInvisible: true, neighbors: [1006, 1010] },
+  { title: "1007", id: 1007, x: 1440, y: 720, isInvisible: true, neighbors: [1006, 1008] },
+  { title: "1008", id: 1008, x: 1435, y: 650, isInvisible: true, neighbors: [1007, 1009] },
+  { title: "1009", id: 1009, x: 1525, y: 640, isInvisible: true, neighbors: [1008] },
+  { title: "1010", id: 1010, x: 1185, y: 630, isInvisible: true, neighbors: [1006.1] },
+  { title: "1011", id: 1011, x: 1110, y: 695, isInvisible: true, neighbors: [1005, 1005.2, 1012] },
+  { title: "1012", id: 1012, x: 1095, y: 595, isInvisible: true, neighbors: [1010, 1011, 1017] },
+  { title: "1013", id: 1013, x: 838, y: 720, isInvisible: true, neighbors: [1003.6] },
+  { title: "1013.1", id: 1013.1, x: 860, y: 710, isInvisible: true, neighbors: [1013] },
+  { title: "1013.2", id: 1013.2, x: 865, y: 680, isInvisible: true, neighbors: [1013.1, 1015.1] },
+  { title: "1014", id: 1014, x: 826, y: 693, isInvisible: true, neighbors: [1013] },
+  { title: "1015", id: 1015, x: 820, y: 670, isInvisible: true, neighbors: [1013] },
+  { title: "1015.1", id: 1015.1, x: 850, y: 665, isInvisible: true, neighbors: [1015] },
+  { title: "1015.2", id: 1015.2, x: 785, y: 575, isInvisible: true, neighbors: [1016] },
+  { title: "1016", id: 1016, x: 795, y: 605, isInvisible: true, neighbors: [1015] },
+  { title: "1016.1", id: 1016.1, x: 980, y: 535, isInvisible: true, neighbors: [1016, 1017] },
+  { title: "1016.2", id: 1016.2, x: 680, y: 652, isInvisible: true, neighbors: [1016, 1016.3] },
+  { title: "1016.3", id: 1016.3, x: 583, y: 675, isInvisible: true, neighbors: [1016.2, 1018] },
+  { title: "1016.4", id: 1016.4, x: 543, y: 570, isInvisible: true, neighbors: [1016.3] },
+  { title: "1016.5", id: 1016.5, x: 520, y: 515, isInvisible: true, neighbors: [1016.4] },
+  { title: "1016.6", id: 1016.6, x: 510, y: 480, isInvisible: true, neighbors: [1016.5, 1020] },
+  { title: "1017", id: 1017, x: 1075, y: 495, isInvisible: true, neighbors: [1012] },
+  { title: "1018", id: 1018, x: 498, y: 710, isInvisible: true, neighbors: [1016.3] },
+  { title: "1018.1", id: 1018.1, x: 515, y: 745, isInvisible: true, neighbors: [1018] },
+  { title: "1018.2", id: 1018.2, x: 470, y: 625, isInvisible: true, neighbors: [1018] },
+  { title: "1019", id: 1019, x: 585, y: 920, isInvisible: true, neighbors: [1018.1] },
+  { title: "1020", id: 1020, x: 425, y: 510, isInvisible: true, neighbors: [1018.2] },
+  { title: "1021", id: 1021, x: 385, y: 400, isInvisible: true, neighbors: [1020] },
+  { title: "E", id: 1013.3, x: 895, y: 720, isInvisible: true, neighbors: [1013.1] },
   { title: "E", id: 1013.4, x: 905, y: 690, isInvisible: true, neighbors: [15] },
   
 
